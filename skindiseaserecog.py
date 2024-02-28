990944886cae621a8925453b90ec68ca101aec80
aswininodagala <148947464+aswininodagala@users.noreply.github.com>
2024-02-28 11:06:58 +0530

    skindiseaserecog.py

---
+++ skindiseaserecog.py
@@ -0,0 +1,77 @@
+### Disease Recognition APP
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+
+
+genai.configure(api_key=st.secrets['API_KEY']) 
+
+## Function to load Google Gemini Pro Vision API And get response
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="Disease Recognition App")
+
+st.header("Disease Recognition App")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Cure My Disease")
+
+input_prompt="""
+You are an expert in dermatology where you need to see the disease of skin from the image
+               and tell how to cure the disease, also provide the details of medicines to use along with diet to follow
+               is below format
+
+               1. Name of the disease
+               2. Reason why disease occured
+               3. what are the medicines need to use
+               4. What is the diet to follow to avoid any rashes and irritations
+               ----
+               ----
+               ----
+               ----
+               ----
+
+
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
